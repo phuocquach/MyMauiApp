22e4a98f21a0445673ba4a578a03d2475811c082
Phuoc <Phuoc.Quach@positivethinking.tech>
2022-04-12 15:23:41 +0700

    Add project files.

---
+++ MyMauiApp/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MyMauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        window.Created += (s, e) =>
+        {
+            // Custom logic
+        };
+
+        return window;
+    }
+}
